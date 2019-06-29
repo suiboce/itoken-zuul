df6b6649bb7be02c2aa893a3fb25d913fa1468d3
17396387378 <515030185@qq.com>
2019-06-29 10:07:08 +0800

    基本开发环境版

---
+++ src/main/java/com/shyb/itoken/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.shyb.itoken.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author wzh
+ * @date 2019/6/28 - 11:23
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
